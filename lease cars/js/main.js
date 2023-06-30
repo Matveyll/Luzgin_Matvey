a0347281900dba403f551ad2a4d711e1e8a41a9a
Luzgin Matvey <luzgin_matvey@mail.ru>
2023-06-30 22:36:27 +0300

    78942234jkdsfs,.s df

--- lease cars/js/main.js
+++ lease cars/js/main.js
@@ -1,4 +1,31 @@
-const swiper = new Swiper(".swiper", {
+
+  const tabItem = document.querySelectorAll('.tabs__btn-item');
+  const tabContent = document.querySelectorAll('.tabs__content-item');
+
+  tabItem.forEach(function(element) {
+    element.addEventListener('click', open);
+  })
+
+  function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function(item){
+      item.classList.remove('tabs__btn-item--active');
+    });
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function(item){
+      item.classList.remove('tabs__content-item--active');
+    });
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+  }
+
+
+
+  const swiper = new Swiper(".swiper", {
     effect: "fade",
     pagination: {
       el: ".swiper-pagination",
