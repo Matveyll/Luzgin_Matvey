6695d18e9f353583cae9f4a184016c4f1f0cbcec
Luzgin Matvey <luzgin_matvey@mail.ru>
2023-07-01 08:27:30 +0300

    ghreyyfergerfsdasjmad

--- lease cars/js/main.js
+++ lease cars/js/main.js
@@ -23,6 +23,17 @@
     document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
   }
 
+  const menuBtn = document.querySelector('.menu__btn');
+  const menu = document.querySelector('.menu__list');
+  
+
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
+  });
+
+
+
 
 
   const swiper = new Swiper(".swiper", {
